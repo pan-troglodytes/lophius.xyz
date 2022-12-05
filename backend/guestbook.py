6a3e8bfb34209d1f312b14fa8c194c86bffb4ff7
sacha <pantroggy@protonmail.com>
2022-12-05 22:23:13 +0000

    improved guestbook validation and error feedback, fixed broken link in gallery

--- backend/guestbook.py
+++ backend/guestbook.py
@@ -18,18 +18,22 @@ guestbookPath = "/var/www/lophius.xyz/guestbook.html"
 guestbookMessagePath = "/var/www/lophius.xyz/guestbook-message.html"
 guestbookMessageAttributePath = "/var/www/lophius.xyz/guestbook-message-attribute.html"
 guestbookMessageAttributeLinkPath = "/var/www/lophius.xyz/guestbook-message-attribute-link.html"
+guestbookFormErrorPath = "/var/www/lophius.xyz/guestbook-form-error.html"
 
-with open(guestbookPath ,"r") as f1, open(guestbookMessagePath ,"r") as f2, open(guestbookMessageAttributePath ,"r") as f3, open(guestbookMessageAttributeLinkPath ,"r") as f4:
+with open(guestbookPath ,"r") as f1, open(guestbookMessagePath ,"r") as f2, open(guestbookMessageAttributePath ,"r") as f3, open(guestbookMessageAttributeLinkPath ,"r") as f4, open(guestbookFormErrorPath,"r") as f5:
     websiteTemplate = f1.read()
     messageTemplate = f2.read()
     attributeTemplate = f3.read()
     attributeLinkTemplate = f4.read()
+    formError = f5.read()
 
 # remove trailing newline chars
 websiteTemplate= websiteTemplate[:len(websiteTemplate)-1]
 messageTemplate= messageTemplate[:len(messageTemplate)-1]
 attributeTemplate = attributeTemplate[:len(attributeTemplate)-1]
 attributeLinkTemplate= attributeLinkTemplate[:len(attributeLinkTemplate)-1]
+formError= formError[:len(formError)-1]
+error = False
 
 
 form = cgi.FieldStorage()
@@ -40,6 +44,7 @@ db = mysql.connector.connect(
     passwd = dbLogin["GUESTBOOK_PASSWD"],
     database = dbLogin["GUESTBOOK_DATABASE"]
 )
+tableName = "Messages";
 mycursor = db.cursor()
 
 if form.getvalue("message") and form.getvalue("name"):
@@ -49,26 +54,47 @@ if form.getvalue("message") and form.getvalue("name"):
     showEmail = False
     website = None
     style = "message-style1"
+    if len(form.getvalue("name")) > 255:
+        websiteTemplate = websiteTemplate.replace("<!--%ERROR_NAME-->", formError.replace("<!--%ERROR-->","invalid name. your name cant be greater than 255 characters"))
+        error = True
+    if len(form.getvalue("message")) > 255:
+        websiteTemplate = websiteTemplate.replace("<!--%ERROR_MESSAGE-->", formError.replace("<!--%ERROR-->","invalid message. your message cant be greater than 255 characters"))
+        error = True
+
     if form.getvalue("email"):
-        email = form.getvalue("email")
+        if re.search("([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+", form.getvalue("email")):
+            email = form.getvalue("email")
+        else:
+            websiteTemplate = websiteTemplate.replace("<!--%ERROR_EMAIL-->", formError.replace("<!--%ERROR-->","invalid email"))
+            error = True
+
     if form.getvalue("show-email"):
-        if (form.getvalue("show-email")) == "on" and re.search("([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+", form.getvalue("email")):
+        if (form.getvalue("show-email")) == "on":
             showEmail = True
-    if form.getvalue("website") and re.search("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", form.getvalue("website")):
-        website = form.getvalue("website")
+    if form.getvalue("website"):
+        if re.search("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", form.getvalue("website")):
+            website = form.getvalue("website")
+        else:
+            websiteTemplate = websiteTemplate.replace("<!--%ERROR_WEBSITE-->", formError.replace("<!--%ERROR-->","invalid website"))
+            error = True
+
     if form.getvalue("message-style"):
         if form.getvalue("message-style") == "message-style2" or form.getvalue("message-style") == "message-style3":
             style = form.getvalue("message-style")
-    mycursor.execute("select * from Messages where Message = %s and Name = %s", (message, name))
+    mycursor.execute("select * from "+tableName+" where Message = %s and Name = %s", (message, name))
     mycursor.fetchall()
-    if mycursor.rowcount == 0:
+    if mycursor.rowcount > 0:
+        websiteTemplate = websiteTemplate.replace("<!--%ERROR_DUPLICATE-->", formError.replace("<!--%ERROR-->","error! a guestbook entry of the exact same name and message already excists, a user cannot send duplicate messages"))
+        error = True
+        
+    if not error:
         date = datetime.datetime.now(timezone('GMT'))
         dateFormatted = str(date.year)+"-"+str(date.month)+"-"+str(date.day)+" "+str(date.hour)+":"+str(date.minute)+":"+str(date.second)
-        mycursor.execute("insert into Messages(Name, Message, Email, ShowEmail, Website, Style, Date) values (%s, %s, %s, %s, %s, %s, %s)", (name, message, email, showEmail, website, style, date))
+        mycursor.execute("insert into "+tableName+"(Name, Message, Email, ShowEmail, Website, Style, Date) values (%s, %s, %s, %s, %s, %s, %s)", (name, message, email, showEmail, website, style, date))
         db.commit();
 
 
-num = mycursor.execute("select * from Messages")
+num = mycursor.execute("select * from "+tableName+"")
 websitesQuantity = 0
 
 for i in mycursor:
@@ -104,7 +130,7 @@ for i in mycursor:
         messageNew = messageNew.replace("<!--%EMAIL-->", email)
     
     websiteTemplate = websiteTemplate.replace("<!--%NEW_MESSAGE-->", messageNew)
-    
+
 websiteTemplate = websiteTemplate.replace("<!--%WEBSITESNO-->", str(websitesQuantity))
 
 print("Content-type:text/html\r\n\r\n")
